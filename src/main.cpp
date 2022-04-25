04f9d2ab4f0edc41d6d9de05807469370d6028dd
Christopher Connolly <cconnolly@pc7c6x2mdw.lan>
2022-04-26 00:14:20 +1000

    E2E Working

--- src/main.cpp
+++ src/main.cpp
@@ -3,71 +3,90 @@
   Copyright (c) 2022 Christopher Connolly
   Project:     Gumball actuator
   Author:      Christopher Connolly
-  Last update: 14.04.2022
-  Description: ESP32 Gumball machine, listens for websocket instructions
+  Last update: 25.04.2022
+  Description: ESP8266 Gumball machine, listens for websocket instructions
   License:     GNU GPL 3, see libraries for respective licenes too
   ============================================================
 */
+
 #include <Arduino.h>
-#include <WiFiMulti.h>
-#include <AsyncElegantOTA.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>      // Include the ESP8266 Wi-Fi library
+#include <ESP8266WiFiMulti.h> // Include the ESP8266 Wi-Fi-Multi library
+#include <ESP8266mDNS.h>      // Include the ESP8266 mDNS library
+#elif defined(ESP32)
+#include <WiFiMulti.h>       // Include the ESP32 Wi-Fi-Multi Library
+#include <ESPmDNS.h>         // Include the ESP32 mDNS Library
+#include <SPIFFS.h>          // Include the ESP32 SPIFFS Library
+#include <AsyncElegantOTA.h> // Include ElegantOTA library for ESP32 only
+#endif
+
 #include <ESPAsyncWebServer.h>
 #include <ESPAsync_WiFiManager.h>
-#include <SPIFFS.h>
-#include <ESPmDNS.h>
 #include <ArduinoWebsockets.h>
 #include <ArduinoJson.h>
 
-
 // Hardcoded stuff
-#define OTA_USERNAME                "twilio"           // Username for OTA firmware updates
-#define OTA_PASSWORD                "tw1l10ns"         // Password for OTA firmware updates
+#define OTA_USERNAME "twilio"   // Username for OTA firmware updates
+#define OTA_PASSWORD "tw1l10ns" // Password for OTA firmware updates
 
-// #define MOTOR_A_PIN                 5                   // Use LED as indicator for now
-#define MOTOR_A_PIN                 2                   // Use LED as indicator for now
-#define WIFICHECK_INTERVAL          5000L               // How often to check WIFI connection
-#define CONFIG_WIFI_PIN             15                  // Built in button on ESP-Dev is 0, use 15 for production boards
-#define BUTTONCHECK_INTERVAL        10000L              // How often to check for button press
+// #define MOTOR_A_PIN 2               // Use LED as indicator for now
+#define WIFICHECK_INTERVAL 5000L    // How often to check WIFI connection
+#define BUTTONCHECK_INTERVAL 10000L // How often to check for button press
 
-// Web Server
-AsyncWebServer                      webServer(80);
+// WIFI Multi
+#if defined(ESP8266)
+// Use wifiMulti
+ESP8266WiFiMulti wifiMulti; // Use ESP8266 WifiMulti
+#define MOTOR_A_PIN 2       // GPIO 2
+#define CONFIG_WIFI_PIN 0   // GPIO 0
+#elif defined(ESP32)        // Use ESP32 WifiMulti
+// Use wifiMulti
+WiFiMulti wifiMulti;
+// PINS
+#define MOTOR_A_PIN 25      // Use LED as indicator for now
+#define CONFIG_WIFI_PIN 0   // Built in button on ESP32-Dev is 0, use 15 for production boards
+#endif
+
+AsyncWebServer webServer(80);
+DNSServer dnsServer;
+String unique_hostname = "Twilio_Gumball_" + String(ESP_getChipId(), HEX); // Suffix AP name with Chip ID
+ESPAsync_WiFiManager ESPAsync_wifiManager(&webServer, &dnsServer, unique_hostname.c_str());
 
 // Connectivitiy
-bool initOtaComplete                = false;
-bool initialConfig                  = false;
-bool initWSComplete                 = false;
-bool isWSConnected                  = false;
-static ulong checkbutton_timeout    = 0;
-static ulong checkwifi_timeout      = 0;
-DNSServer                           dnsServer;
-String unique_hostname              = "Twilio_Gumball_" + String(ESP_getChipId(), HEX);   // Suffix AP name with Chip ID
-ESPAsync_WiFiManager                ESPAsync_wifiManager(&webServer, &dnsServer, unique_hostname.c_str());
+bool initOtaComplete = false;
+bool initialConfig = false;
+bool initWSComplete = false;
+bool isWSConnected = false;
+
+static ulong checkbutton_timeout = 0;
+static ulong checkwifi_timeout = 0;
 
 // Configurable parameters in the UI
-const char *ws_url                  = "wss://localhost/wsconnect";
-websockets::WebsocketsClient        wsClient;
+const char *ws_url = "ws://192.168.1.215:5001/";
+websockets::WebsocketsClient wsClient;
 
 // #define LABEL_WS_URL               "WS Host URL"
 // #define LABEL_DISPENSE_DURATION     "Dispense DurationL"
 // ESPAsync_WMParameter                p_mqtt_url(LABEL_WS_URL, LABEL_WS_URL, "XXX", 255);
 // ESPAsync_WMParameter                p_dispense_duration(LABEL_DISPENSE_DURATION, LABEL_DISPENSE_DURATION, "YYY", 1);
 
-// Use wifiMulti
-WiFiMulti wifiMulti;
-
 /* This variables stores the duration in milliseconds to dispense candy. Longer = more candy!!! */
 int dispense_duration = 4000L;
 
-void giveMeCandy() {
+void giveMeCandy()
+{
   // Serial.printf("Dispensing candy!\n");
   digitalWrite(MOTOR_A_PIN, HIGH);
   delay(dispense_duration);
   digitalWrite(MOTOR_A_PIN, LOW);
 }
 
-
-void connectOrConfigureWifi() {
-  if (initialConfig) {
+void connectOrConfigureWifi()
+{
+  if (initialConfig)
+  {
     Serial.println(F("Inital Config mode. Starting Portal"));
     digitalWrite(LED_BUILTIN, HIGH);
 
@@ -82,13 +101,15 @@ void connectOrConfigureWifi() {
     // Set timeout on configuration portal
     ESPAsync_wifiManager.setConfigPortalTimeout(30);
 
-    if (!ESPAsync_wifiManager.startConfigPortal(unique_hostname.c_str(), OTA_PASSWORD)) {
+    if (!ESPAsync_wifiManager.startConfigPortal(unique_hostname.c_str(), OTA_PASSWORD))
+    {
       Serial.println(F("Not connected to WiFi"));
-    } else {
+    }
+    else
+    {
       Serial.println(F("Wifi connected"));
       String Router_SSID = ESPAsync_wifiManager.WiFi_SSID();
       String Router_Pass = ESPAsync_wifiManager.WiFi_Pass();
-      Serial.println("WIFI Manage storing SSID = " + Router_SSID + ", Pass = " + Router_Pass);
       wifiMulti.addAP(Router_SSID.c_str(), Router_Pass.c_str());
       WiFi.mode(WIFI_STA);
       WiFi.begin();
@@ -96,49 +117,76 @@ void connectOrConfigureWifi() {
 
     initialConfig = false;
     digitalWrite(LED_BUILTIN, LOW);
-
-  } else {
-    Serial.println(F("Normal mode. Entering WIFI_STA mode"));
-    WiFi.mode(WIFI_STA);
-    WiFi.begin();
+  }
+  else
+  {
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println(F("Normal mode. Entering WIFI_STA mode"));
+      WiFi.mode(WIFI_STA);
+      WiFi.begin();
+      int retryCount = 0;
+      while (WiFi.status() != WL_CONNECTED && retryCount < 20)
+      {
+        retryCount++;
+        Serial.print('.');
+        delay(1000);
+      }
+    }
+    else
+    {
+      Serial.print(F("WIFI Connected. IP:"));
+      Serial.println(WiFi.localIP());
+    }
   }
 }
-
 
 void onWsEventsCallback(websockets::WebsocketsEvent event, String data)
 {
   if (event == websockets::WebsocketsEvent::ConnectionOpened)
   {
-    Serial.println("Connnection Opened");
+    Serial.println(F("WS Connnection Opened"));
   }
   else if (event == websockets::WebsocketsEvent::ConnectionClosed)
   {
-    Serial.println("Connnection Closed");
+    Serial.println(F("WS Connnection Closed"));
     isWSConnected = false;
   }
   else if (event == websockets::WebsocketsEvent::GotPing)
   {
-    Serial.println("Got a Ping!");
+    Serial.println(F("WS Got a Ping!"));
   }
   else if (event == websockets::WebsocketsEvent::GotPong)
   {
-    Serial.println("Got a Pong!");
+    Serial.println(F("WS Got a Pong!"));
   }
   else
   {
-    Serial.println("Got unknown event");
+    Serial.println(F("WS Got unknown event"));
   }
 }
 
 void onWsMessageCallback(websockets::WebsocketsMessage message)
 {
-  Serial.printf("<<< Got Message:\n%s\n\n", message.data().c_str());
-  try {
+  Serial.printf("<<< Got WS Message:\n%s\n\n", message.data().c_str());
+  try
+  {
     StaticJsonDocument<500> json;
     if (deserializeJson(json, message.data().c_str()) == DeserializationError::Ok)
     {
-      //TODO: Somthing with the message
-      giveMeCandy();
+      JsonObject object = json.as<JsonObject>();
+      JsonVariant action = object.getMember("action");
+      if (action.isNull())
+      {
+        Serial.println(F("Error: action is missing"));
+      }
+      else
+      {
+        if (strcmp(action.as<const char *>(), "dispense") == 0)
+        {
+          giveMeCandy();
+        }
+      }
     }
     else
     {
@@ -146,66 +194,77 @@ void onWsMessageCallback(websockets::WebsocketsMessage message)
     }
   }
   catch (std::exception const &e)
-    {
-      printf("Message Processing Error: %s\n", e.what());
-    }
+  {
+    printf("Message Processing Error: %s\n", e.what());
+  }
 }
 
-void connectOrConfigureWebsocket() {
+void connectOrConfigureWebsocket()
+{
   // Exit if already initialized
-  if (isWSConnected) return;
+  if (isWSConnected)
+    return;
   /* TODO:
-  1. Get WS Url
-  2. Wire receiver function
-  3. Digest message
-  4. Connect to host
+  1. Get WS Url from configuration
   */
- 
- isWSConnected = wsClient.connect(ws_url);   // Establish the web socket connection
+  wsClient = {};                           // Reset web socket client
+  wsClient.onMessage(onWsMessageCallback); // Wire events callback
+  wsClient.onEvent(onWsEventsCallback);    // Wire message callback
+
+  isWSConnected = wsClient.connect(ws_url); // Establish the web socket connection
 }
 
-void initOTA() {
+#if defined(ESP32)
+void initOTA()
+{
   // Exit if already initialized
-  if (initOtaComplete) return;
+  if (initOtaComplete)
+    return;
 
   /* Advertise us with MDNS */
-  if (!MDNS.begin(unique_hostname.c_str())) { // http://<DEVICE_NAME>.local
+  if (!MDNS.begin(unique_hostname.c_str()))
+  { // http://<DEVICE_NAME>.local
     Serial.println(F("Error setting up MDNS responder!"));
-  } else {
+  }
+  else
+  {
     Serial.println("mDNS configured: " + String(unique_hostname) + ".local");
   }
 
-  webServer.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html","text/html");
-  });
+  webServer.on("/index.html", HTTP_GET, [](AsyncWebServerRequest *request)
+               { request->send(SPIFFS, "/index.html", "text/html"); });
 
-  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html","text/html");
-  });
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+               { request->send(SPIFFS, "/index.html", "text/html"); });
 
-  webServer.onNotFound([](AsyncWebServerRequest *request){
-    request->send(404);
-  });
-    
+  webServer.onNotFound([](AsyncWebServerRequest *request)
+                       { request->send(404); });
+
   // Configure ElegantOTA with username and password
   AsyncElegantOTA.begin(&webServer, OTA_USERNAME, OTA_PASSWORD);
   webServer.begin();
   MDNS.addService("http", "tcp", 80);
-  
+
   Serial.println(F("HTTP server started"));
   initOtaComplete = true;
 }
+#endif
 
-void checkWifiLoop() {
+void checkWifiLoop()
+{
   static ulong current_millis;
   current_millis = millis();
 
   // Check WiFi every WIFICHECK_INTERVAL seconds.
   if ((current_millis > checkwifi_timeout) || (checkwifi_timeout == 0))
   {
-    if ((WiFi.status() != WL_CONNECTED) || initialConfig) {
+    if ((WiFi.status() != WL_CONNECTED) || initialConfig)
+    {
       Serial.println(F("WIFI not connected"));
-    } else {
+      connectOrConfigureWifi(); // Connect to WIFI
+    }
+    else
+    {
       Serial.print(F("WIFI Connected. Local IP: "));
       Serial.println(WiFi.localIP());
       /*
@@ -213,16 +272,19 @@ void checkWifiLoop() {
         otherwise we will be unable to update firmware, calling initOTA
         before having a WIFI connection will cause the ESP to crash
       */
+#if defined(ESP32)
       initOTA();
-      connectOrConfigureWifi();                   // Connect to WIFI
-      connectOrConfigureWebsocket();              // Connect to WS
+#endif
+      connectOrConfigureWebsocket(); // Connect to WS
     }
     checkwifi_timeout = current_millis + WIFICHECK_INTERVAL;
   }
 }
 
-void blinkLed(int led, int blinkDelay, int cycles) {
-  for (int i = 0; i < cycles; i++) {
+void blinkLed(int led, int blinkDelay, int cycles)
+{
+  for (int i = 0; i < cycles; i++)
+  {
     digitalWrite(led, HIGH);
     delay(blinkDelay);
     digitalWrite(led, LOW);
@@ -230,7 +292,8 @@ void blinkLed(int led, int blinkDelay, int cycles) {
   }
 }
 
-void checkButtons() {
+void checkButtons()
+{
   static ulong current_millis;
   current_millis = millis();
 
@@ -261,27 +324,33 @@ void checkButtons() {
   1. Check we're connected
 */
 
-void setup() {
+void setup()
+{
   // MOTORS AND LEDS
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MOTOR_A_PIN, OUTPUT);
   pinMode(CONFIG_WIFI_PIN, INPUT_PULLUP);
-  // pinMode(CONFIG_WIFI_PIN, INPUT);  
+  // pinMode(CONFIG_WIFI_PIN, INPUT);
 
   // set-up serial for debug output
-  Serial.begin(115200); while (!Serial); delay(200);
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  delay(200);
   Serial.printf("\nStarting Connected Gumball Feeder\n");
-
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
+#if defined(ESP32)
+  if (!SPIFFS.begin())
+  {
+    Serial.println(F("An Error has occurred while mounting SPIFFS"));
     return;
   }
-
-  wsClient.onMessage(onWsMessageCallback);    // Wire events callback
-  wsClient.onEvent(onWsEventsCallback);       // Wire message callback
+#endif
 }
 
-void loop() {
-    checkButtons();                           // Check if buttons have been pressed
-    checkWifiLoop();                          // Ensure WIFI connected
+void loop()
+{
+  checkButtons();  // Check if buttons have been pressed
+  checkWifiLoop(); // Ensure WIFI connected
+  wsClient.poll(); // Check for incoming messages
+  yield();
 }
